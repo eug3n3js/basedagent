cf1e9533f790b914d14f17089e29aabc222c8e64
eug3n3js <eug3n3js@example.com>
2025-10-26 18:20:52 +0100

    fixed redirect

--- src/main/routers/chat_router.py
+++ src/main/routers/chat_router.py
@@ -25,6 +25,7 @@ async def get_task_types(current_user: AccessData = Depends(get_access_data),
     task_types = chat_service.get_task_types()
     return JSONResponse(content=jsonable_encoder(task_types, exclude_none=True))
 
+
 @chat_router.get("/{chat_id}")
 async def get_chat(chat_id: int,
                    current_user: AccessData = Depends(get_access_data),
@@ -32,15 +33,6 @@ async def get_chat(chat_id: int,
     await chat_service.verify_chat_ownership(chat_id, current_user.sub)
     chat = await chat_service.get_by_id(chat_id)
     return JSONResponse(content=jsonable_encoder(chat, exclude_none=True))
-
-@chat_router.get("/")
-async def get_user_chats(limit: int = Query(50, ge=1, le=100),
-                         offset: int = Query(0, ge=0),
-                         current_user: AccessData = Depends(get_access_data),
-                         chat_service: ChatService = Depends(ChatService.get_instance)) -> JSONResponse:
-    chats = await chat_service.get_user_chats(current_user.sub, limit, offset)
-    return JSONResponse(content=jsonable_encoder(chats, exclude_none=True))
-
 
 
 @chat_router.get("/{chat_id}/status")
@@ -50,6 +42,7 @@ async def get_chat_status(chat_id: int,
     is_pending = await chat_service.is_chat_pending(chat_id)
     
     return JSONResponse(content={"is_pending": is_pending})
+
 
 @chat_router.get("/{chat_id}/messages")
 async def get_chat_messages(chat_id: int,
@@ -89,3 +82,12 @@ async def process_message_task(message_create: MessageCreate,
             exclude_none=True
         )
     )
+
+
+@chat_router.get("/")
+async def get_user_chats(limit: int = Query(50, ge=1, le=100),
+                         offset: int = Query(0, ge=0),
+                         current_user: AccessData = Depends(get_access_data),
+                         chat_service: ChatService = Depends(ChatService.get_instance)) -> JSONResponse:
+    chats = await chat_service.get_user_chats(current_user.sub, limit, offset)
+    return JSONResponse(content=jsonable_encoder(chats, exclude_none=True))
